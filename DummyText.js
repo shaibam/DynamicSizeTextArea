be9293e0a9e68e5709bde84c49f0e636a0379b0e
shaibam <shaibam@gmail.com>
2018-01-06 21:12:17 +0200

    changet to div contentEditable proprty

--- DummyText.js
+++ DummyText.js
@@ -1,13 +1,51 @@
 import React from "react";
-import TextAreaStyle from "./TextAreaStyle.css";
 export default class DummyText extends React.Component {
+    static defaultProps = {
+		onHasChaged: ()=>{}
+	}
     constructor(props) {
         super(props);
+        this.state={style:{
+            /*top:'0px',
+            position:'absolute',
+            display:'block',*/
+            position:'absolute',
+            height:'15px',
+            /*overflow:'hidden',*/
+            resize:'none',
+            top:'0px',
+            //transform:'translateY(0px)',
+            width:"100%",
+            background:"white"
+        }}
     }
-    onChange = () =>{
-
+    onInput = (e) =>{
+        this.forceUpdate();
     }
     render() {
-        return <textarea rows={1} onChange={this.onChange}></textarea>
+        //console.log('render',this.state.style)
+        //return <textarea ref='textarea'rows={1} onChange={this.onChange} style={this.state.style}></textarea>
+        return <div ref='textarea'contentEditable="true" onInput={this.onInput} style={this.state.style}></div>
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        //dont render if height of textarea hasn't changed
+        /*console.log(nextProps.topBy,nextState.style.transform.replace('translateY(','').replace('px)',''));
+        if (nextProps.topBy && nextProps.topBy!=nextState.style.transform.replace('translateY(','').replace('px)','')) {
+            console.log(nextProps.topBy,nextState.style.transform.replace('translateY(','').replace('px)',''))
+            nextState.style.transform='translateY('+nextProps.topBy+'px)';
+            return true;
+        }*/
+        if (this.state.style.height == nextState.style.height) return false
+        if (this.state.style.height!='auto')
+            this.props.onHasChaged(nextState.style.height);
+		return true
+	}
+    componentDidUpdate() {
+        var state ={...this.state}
+        var style ={...state.style}
+        //console.log(style)
+        style.height = this.refs.textarea.scrollHeight;
+        state.style=style;
+        this.setState(state);
     }
 }
--- TextArea.js
+++ TextArea.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DummyText from "./DummyText.js"
-import TextAreaStyle from "./TextAreaStyle.css";
+//import TextAreaStyle from "./TextAreaStyle.css";
 export default class TextArea extends React.Component {
 	//tip found at https://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize?page=1&tab=votes#tab-top
 	static defaultProps = {
@@ -9,20 +9,40 @@ export default class TextArea extends React.Component {
 	}
 	constructor(props) {
 		super(props);
-		//console.log(TextAreaStyle)
-		this.miniScrollHeight = undefined;
+		this.rowHeight = 17; //assumption
 		this.state = {
-			rows: 1,
-			maxRowReached: false,
-			textValue: '',
 			style: {
-				resize: "none",
 				overflow: "hidden",
-				height: "auto"
-			}
+				display: "block",
+				position: "relative",
+				height: "30px",
+				width:"100%"
+			}/*,
+			pushTopBy: '0px'*/
 		}
 	}
+	onHasChanged = (height) => {
+		//console.log('onHasChanged', height);
+		var state = { ...this.state };
+		var style = { ...state.style };
+		setTimeout(() => {
+			if (height < this.props.maxRows * this.rowHeight) {
+				style.height = height;
+				//state.pushTopBy = '0px';
+			} else {
+				style.height = this.props.maxRows * this.rowHeight;
+				//state.pushTopBy = height - style.height;
+				//console.log('state.pushTopBy', state.pushTopBy)
+			}
+			state.style = style;
+			//console.log('state',state);
+			this.setState(state);
+		}, 0)
+	}
 	render() {
-		return <DummyText></DummyText>
+		console.log('render', this.state.pushTopBy);
+		return <div style={this.state.style}>
+			<DummyText minRows={this.props.minRows} maxRows={this.props.maxRows} onHasChaged={this.onHasChanged} topBy={this.state.pushTopBy}></DummyText>
+		</div>
 	}
 }
